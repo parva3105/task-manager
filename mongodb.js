2e49a1ca7b409891d2c182dc3dc95bf450f0cf52
Parva Shah <parvashah310501@gmail.com>
2021-10-22 18:08:04 +0530

    Initial Commit , Insert documents via code

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+// CRUD Create Read Update Delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser : true } , (error , client) => {
+    if(error) {
+        return console.log('Unable to connect to Database !');
+    }    
+
+    const db = client.db(databaseName)
+    db.collection('Users').insertOne({
+        name: 'Parva Shah',
+        age : 20
+    })
+})
