7c7765bacabd57a27de8551f47579e066de9e463
Parva Shah <parvashah310501@gmail.com>
2021-10-23 20:31:42 +0530

    get the binary string bufer

--- mongodb.js
+++ mongodb.js
@@ -10,8 +10,7 @@ const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectId()
-console.log(id);
-console.log(id.getTimestamp());
+console.log(id.id);
 
 MongoClient.connect(connectionURL, { useNewUrlParser : true } , (error , client) => {
     if(error) {
