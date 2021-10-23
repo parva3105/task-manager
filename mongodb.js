8451e50a9dcf91e74777c968d507cbe29aef5f8c
Parva Shah <parvashah310501@gmail.com>
2021-10-23 20:30:22 +0530

    Create New ID on self

--- mongodb.js
+++ mongodb.js
@@ -1,27 +1,35 @@
 // CRUD Create Read Update Delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectId = mongodb.ObjectId
+
+const { MongoClient , ObjectId} = require('mongodb') //Shorthand Expression
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectId()
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewUrlParser : true } , (error , client) => {
     if(error) {
         return console.log('Unable to connect to Database !');
     }    
 
-    const db = client.db(databaseName)
+    // const db = client.db(databaseName)
 
-//     db.collection('Users').insertOne({
-//         name: 'Parva',
-//         age : 20
-//     } , (error , result) => {
-//         if (error) {
-//             return console.log('Unable to Insert user data');
-//         }
-//         console.log(result.insertedId);
-//     })
+    //  db.collection('Users').insertOne({
+    //      _id : id,
+    //      name: 'Parva',
+    //      age : 20
+    //  } , (error , result) => {
+    //      if (error) {
+    //          return console.log('Unable to Insert user data');
+    //      }
+    //      console.log(result.insertedId);
+    //  })
     // db.collection('Users').insertMany([
     //     {
     //         name : 'Jen',
@@ -36,23 +44,23 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true } , (error , client)
     //     }
     //     console.log(result.insertedIds);
     // })
-    db.collection('Tasks').insertMany([
-        {
-            description : 'Complete the Course',
-            completed : false 
-        } , {
-            description : 'Go to Birthday',
-            completed : false
-        }, {
-            description : 'Pick up uncle',
-            completed : true 
-        }
-    ] , (error , result) => {
-        if (error){
-            return console.log('Unable to connect to the database');
-        } 
+    // db.collection('Tasks').insertMany([
+    //     {
+    //         description : 'Complete the Course',
+    //         completed : false 
+    //     } , {
+    //         description : 'Go to Birthday',
+    //         completed : false
+    //     }, {
+    //         description : 'Pick up uncle',
+    //         completed : true 
+    //     }
+    // ] , (error , result) => {
+    //     if (error){
+    //         return console.log('Unable to connect to the database');
+    //     } 
         
-        console.log(result.insertedIds);
-    })
+    //     console.log(result.insertedIds);
+    // })
 
 })
