0a81175e43139e4bc155b1d1693be0fa97087c25
Parva Shah <parvashah310501@gmail.com>
2021-10-26 16:31:43 +0530

    How to search using id

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,11 @@
 // CRUD Create Read Update Delete
 
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectId = mongodb.ObjectId
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const ObjectId = mongodb.ObjectId
 
 //Shorthand Expression
-const { MongoClient , ObjectId } =require('mongodb')
+// const { MongoClient , ObjectId } =require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -18,7 +18,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true } , (error , client)
 
     const db = client.db(databaseName)
 
-    // db.collection('Users').findOne({__id : new ObjectId("61741f6521c029cd95d61583")} ,(error , user) => {
+    // db.collection('Users').findOne({_id : new ObjectId("61741f6521c029cd95d61583")} ,(error , user) => {
     //     if( error ){
     //         return console.log('Unable to Fetch');
     //     }
@@ -26,18 +26,23 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true } , (error , client)
     //     console.log(user);
     // })
 
-    db.collection('Users').find({ age : 20 }).toArray((error , users) => {
+    // db.collection('Users').find({ age : 20 }).toArray((error , users) => {
+    //     if(error){
+    //         return console.log('Unable to fetch');
+    //     }
+    //     console.log(users);
+    // })
+    // db.collection('Users').find({ age : 20 }).count((error , count) => {
+    //     if(error){
+    //         return console.log('Unable to fetch');
+    //     }
+    //     console.log(count);
+    // })
+    db.collection('Tasks').findOne({_id : new ObjectId("6174210f9264aac27ace0345")}, (error , result) => {
         if(error){
             return console.log('Unable to fetch');
         }
-        console.log(users);
-    })
-    db.collection('Users').find({ age : 20 }).count((error , count) => {
-        if(error){
-            return console.log('Unable to fetch');
-        }
-        console.log(count);
-    })
-
+        console.log(result);
+    } )
    
 })
