05168d2a759218a9c63c71b484c441a725b0f6ce
Parva Shah <parvashah310501@gmail.com>
2021-10-29 02:12:36 +0530

    Connected endpoints using postman

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,55 +1,5 @@
 const mongoose = require('mongoose')
-const validator =  require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api' ,{
     useNewUrlParser: true,
-})
-const User = mongoose.model('User',{
-    name: {
-        type: String,
-        required :  true,
-        trim : true
-    },
-    email : {
-        type : String,
-        required : true,
-        trim: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is Invalid')
-            }
-        }
-    }, 
-    password : {
-        type: String,
-        required: true,
-        minLength : 7,
-        trim : true,
-        validate(value){
-            if(value.toLoweCase().includes('password')){
-                throw new Error("Password cannot be password!")
-            }
-        }
-    },
-    age : {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value< 0) {
-                throw new Error('Age must be postive number')
-            }
-        }
-    }
-})
-
-const me = new User({
-    name : '    Parva Shah ',
-    email:'PARVASHAH310501@GMAIL.COM    ',
-    password : 'OkT'
-})
-
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error!!! ' ,error );
 })
---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const validator =  require('validator')
+
+const User = mongoose.model('User',{
+    name: {
+        type: String,
+        required :  true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : true,
+        trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is Invalid')
+            }
+        }
+    }, 
+    password : {
+        type: String,
+        required: true,
+        minLength : 7,
+        trim : true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error("Password cannot be password!")
+            }
+        }
+    },
+    age : {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value< 0) {
+                throw new Error('Age must be postive number')
+            }
+        }
+    }
+})
+
+module.exports = User
