c102ee2751edb422b41dd8c66e29f1a75260acbb
Parva Shah <parvashah310501@gmail.com>
2021-10-30 22:28:47 +0530

    remove -> Delete

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,8 +3,8 @@ const Task = require('../src/models/task')
 
 // 617d7758a6cec7ceba69ebf5
 
-Task.findByIdAndRemove('617d7758a6cec7ceba69ebf5').then((task) =>{
-    console.log(task + ' removed successfully!');
+Task.findByIdAndDelete('617d7758a6cec7ceba69ebf5').then((task) =>{
+    console.log(task);
 
     return Task.countDocuments({ completed : false})
 }).then((result) => {
