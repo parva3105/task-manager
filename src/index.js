dab0a9a6920f3df9c0dbc9e14f486e436633506b
Parva Shah <parvashah310501@gmail.com>
2021-10-31 19:45:22 +0530

    Upadating User

--- src/index.js
+++ src/index.js
@@ -41,6 +41,20 @@ app.get('/users/:id', async (req , res) => {
         res.status(500).send(e)
     }
 })
+
+app.patch('/users/:id' , async(req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body , { new : true, runValidators : true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.post('/tasks' , async(req , res) =>{
     const task = new Task(req.body)
 
