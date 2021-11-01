25784b2742047a392b23646cd48a455c10b19b4c
Parva Shah <parvashah310501@gmail.com>
2021-11-01 22:30:14 +0530

    Added password hashing for users

--- src/index.js
+++ src/index.js
@@ -16,3 +16,18 @@ app.use(taskRouter)
 app.listen(port ,() => {
     console.log("Server up on Port : ", port);
 })
+
+const bcrypt = require('bcryptjs')
+
+// const myFunction = async() => {
+//     const password = 'Red12345!'
+//     const hashedPassword = await bcrypt.hash(password , 8)
+
+//     console.log(password);
+//     console.log(hashedPassword);
+
+//     const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+//     console.log(isMatch);
+
+// }
+// myFunction()
