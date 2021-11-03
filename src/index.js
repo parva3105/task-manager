d4a2752e55fbd833513e7f99f6fce4a21f1f9891
Parva Shah <parvashah310501@gmail.com>
2021-11-03 22:02:24 +0530

    task

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req , res , next) => {
+//     if(req.method === 'GET'){
+//         res.send('GET requests are disabled!')
+//     } else {
+//         next()
+//     }
+// })
+
 app.use((req , res , next) => {
-    if(req.method === 'GET'){
-        res.send('GET requests are disabled!')
-    } else {
-        next()
-    }
+    res.status(503).send('Server Under Maintenance !!')
 })
 
 app.use(express.json())
