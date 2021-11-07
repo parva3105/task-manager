71a86446cb98984516d3b74d8ec079f8b395cb9f
Parva Shah <parvashah310501@gmail.com>
2021-11-08 01:41:21 +0530

    Hiding password and token

--- src/index.js
+++ src/index.js
@@ -25,14 +25,3 @@ app.use(taskRouter)
 app.listen(port ,() => {
     console.log("Server up on Port : ", port);
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async() => {
-    const token = jwt.sign({ _id: 'abc123' },'thisismynewcourse' , {expiresIn : '7 days'})
-    console.log(token);
-
-    const data = jwt.verify(token , 'thisismynewcourse')
-    console.log(data);
-}
-myFunction()
