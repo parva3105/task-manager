a4e9d7cd2c91823e42a9c12c93f04b01503593bc
Parva Shah <parvashah310501@gmail.com>
2021-11-09 16:54:55 +0530

    auth user endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -66,7 +66,7 @@ router.get('/users/:id', async (req , res) => {
     }
 })
 
-router.patch('/users/:id' , async(req, res) => {
+router.patch('/users/me' , auth , async(req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name' , 'email' , 'password' ,'age']  
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -76,16 +76,9 @@ router.patch('/users/:id' , async(req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
-        
-        updates.forEach((update) => user[update] = req.body[update])
-
-        await user.save()
-
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
@@ -93,6 +86,7 @@ router.patch('/users/:id' , async(req, res) => {
 
 router.delete('/users/me', auth ,async(req , res) => {
     try {
+
         await req.user.remove()
         res.send(req.user)
     } catch(e) {
