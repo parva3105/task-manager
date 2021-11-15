1cb709e864e5727a85d6ae70f0ca8f47e145548a
Parva Shah <parvashah310501@gmail.com>
2021-11-15 13:55:30 +0530

    Avatar shaping

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
@@ -108,7 +109,8 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth , upload.single('avatar') , async(req , res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width : 250 , height : 250}).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 } , (error , req , res , next) => {
@@ -129,7 +131,7 @@ router.get('/users/:id/avatar' , async(req , res) => {
             throw new Error()
         }
 
-        res.set('Content-Type' ,'image/jpg')
+        res.set('Content-Type' ,'image/png')
         res.send(user.avatar)
     }catch(e){
         res.status(404).send()
