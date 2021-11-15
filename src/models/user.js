1e327c4b68aa6673f3ad9966dcf45901d920a9d4
Parva Shah <parvashah310501@gmail.com>
2021-11-15 13:38:29 +0530

    Upload and delete avatar

--- src/models/user.js
+++ src/models/user.js
@@ -47,7 +47,9 @@ const userSchema = new mongoose.Schema({
             type : String,
             required : true
         }
-    }]
+    }], avatar : {
+            type : Buffer
+        }
 } , {
     timestamps : true
 })
--- src/routers/user.js
+++ src/routers/user.js
@@ -96,7 +96,6 @@ router.delete('/users/me', auth ,async(req , res) => {
 })
 
 const upload = multer({
-    dest: 'avatar',
     limits : {
         fileSize : 1000000
     }, 
@@ -108,10 +107,18 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar') , (req , res) => {
+router.post('/users/me/avatar', auth , upload.single('avatar') , async(req , res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 } , (error , req , res , next) => {
     res.status(400).send({error : error.message})
+})
+
+router.delete('/users/me/avatar' , auth , async(req , res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 module.exports = router
